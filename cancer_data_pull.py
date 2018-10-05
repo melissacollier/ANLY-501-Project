b84b5fc873217297182bf3a08d3a3c4feee8bfde
Arshia Singh <arshiasingh@ymail.com>
2018-10-04 20:59:30 -0400

    rm footer info from df

--- cancer_data_pull.py
+++ cancer_data_pull.py
@@ -11,7 +11,7 @@ import requests
 
 url = 'https://www.statecancerprofiles.cancer.gov/incidencerates/index.php?stateFIPS=99&cancer=001&race=00&sex=0&age=001&type=incd&sortVariableName=rate&sortOrder=desc&output=1'
 s = requests.get(url).content
-ds = pd.read_csv(io.StringIO(s.decode('windows-1252')), skiprows=8)
+ds = pd.read_csv(io.StringIO(s.decode('windows-1252')), skiprows=8, skipfooter=27, engine='python')
 print(ds.describe())
 
 #split county, state, SEER, NPCR into separate columns
