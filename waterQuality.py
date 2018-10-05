cece23c2de7ebcd9f88fc5d171315a6f00fc9a7c
JJJJJingL <43591006+JJJJJingL@users.noreply.github.com>
2018-10-05 19:04:50 -0400

    Update waterQuality.py

--- waterQuality.py
+++ waterQuality.py
@@ -31,54 +31,74 @@ def main():
     waterDF=pd.DataFrame.from_dict(waterRawdata['pmTableResultWithCWS']) 
     
 #    #### output into .csv file, optional
-#    waterDF.to_csv('waterQuality.csv', sep='\t', encoding='utf-8')
+    waterDF.to_csv('waterQuality.csv', sep='\t', encoding='utf-8')
 
     ### use cleaning data function
     waterResult = waterClean(waterDF)
 
 
-### cleaning data function
+### Data features
 def waterClean(data):
     ### Checking datatypes before cleaning
     print (data.info())
     ### Datatype of dataValue is object, change it into numericals
     data['dataValue'] = pd.to_numeric(data['dataValue'], errors='coerce')
-       
-    ### Checking missing values/typos/outliers in datasets   
-    print(pd.isna(data).sum())
-    print (data.describe().transpose())
-    print (data['display'].value_counts())
-    print (data['dataValue'].value_counts())
-    print (data['title'].value_counts())
+    ### Checking missing values/typos/outliers in datasets  
+    with open ('waterCheck.txt','w') as wc:
+#        wc.write("\nHave a general understanding of the dataset:\n")
+#        wc.write(data.info().to_string())
+        wc.write("Check if there is missing value for each column:\n")
+        wc.write(pd.isna(data).sum().to_string())
+        wc.write("\nOverall review of dataset:\n")
+        wc.write(data.describe().transpose().to_string())
+        wc.write("\nvalue_counts() function: check is there any error value in 'display' colomn\n")
+        wc.write(data['display'].value_counts().to_string())
+        wc.write("\nvalue_counts() function:check is there any error value in 'dataValue' colomn\n")
+        wc.write(data['dataValue'].value_counts().to_string())
+        wc.write("\nvalue_counts() function:check is there any error value in 'title' colomn \n")
+        wc.write(data['title'].value_counts().to_string()) 
+    ### Second method to check missing values/typos/outliers in datasets   
     displayOUT = data['display'].unique()
     dataValueOUT = data['dataValue'].unique()
     titleOUT = data['title'].unique()
     
     ### Cleaning data
-    ### 1. remove non detect value of water quality 
-    data = data[(data['display'] != 'Non Detect')]
+
     ### Datatype of dataValue is object, change it into numericals
     data['dataValue'] = pd.to_numeric(data['dataValue'], errors='coerce')
-    ### 2. get the means of all values for each county using "groupby" method
+    ### 1. get the means of all values for each county using "groupby" method
     result = data.groupby(['title'])['dataValue'].mean()
     ### change "series" into "dataframe" datatype
+    with open ('waterCheck.txt','a') as wc:
+        wc.write("\nThe mean value of arsenic concerntration for each county\n")
+        wc.write(result.to_string())
+        
+    ### change "series" into "dataframe" datatype    
     result = result.to_frame().reset_index()
     result.columns = ['Location','Value']
-    ### split the location column into two column("county","state"), expand=True to add these two column into dataframe
+    
+    ### 2. split the location column into two column("county","state"), expand=True to add these two column into dataframe
     result[['county','state']] =result['Location'].str.split(',', n=1, expand=True)
     ### remove the location column
     del result['Location']
     print (result.describe())
     ### the max value is 31.837500, which is lower than 50
-
+    
+    
+    ### 3. binning the mean into three categories
     ### According to documents in its original website:
     ### level < 1 means non-dect arsenic
     ### level in (1-10) means less than MCL == "no harm"
     ### level in (10-50) means "harmful"
-    ### binning the mean into three categories
     bins = [0,1,10,50]
     labels=['Non Detect','Less than or equal MCL','More than MCL' ]
+    ### 4. remove non detect value of water quality 
+    result = result[(result['Quality'] != 'Non Detect')]
     result['Quality']=pd.cut(result['Value'],bins,labels=labels)
+    with open ('waterCheck.txt','a') as wc:
+        wc.write("\nThe cleaned dataset and new dataset features\n")
+        wc.write(result.to_string())
+    
     return result
     
 main()
