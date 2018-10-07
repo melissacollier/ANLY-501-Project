63b1476dd74104164028323354c5c9bc36c1de2f
Arshia Singh <arshiasingh@ymail.com>
2018-10-07 12:38:28 -0400

    updates for Air

--- combined_datapull.py
+++ combined_datapull.py
@@ -240,14 +240,14 @@ def main():
     genFnsWrapper(allPollutionData, 'Air_data_analysis.txt')
     allPollutionData.to_csv('All_Pollution_Data.csv')
     dataOnlyStateCounty = allPollutionData.loc[:,['State','County']]
-    referenceStateCounty = countyReference.loc[:,['State','County']]
+    referenceStateCounty = countyReference.loc[:,['State','County Name']]
     ##groupby('State') is grouping the dataframe by the unique values in the State column.
     ##The values in Country column are mapped to each unique value from State column.
     ##Tthen index for County column and turn the values that are mapped to the State column into 
     ##each distinct list groupings.
     ##Then turn each unique grouping to a dict
     a = dataOnlyStateCounty.groupby('State')['County'].apply(list).to_dict()
-    b = referenceStateCounty.groupby('State')['County'].apply(list).to_dict()
+    b = referenceStateCounty.groupby('State')['County Name'].apply(list).to_dict()
     columns = ['Year','Days with AQI','Good Days','Moderate Days','Max AQI','90th Percentile AQI',
                'Median AQI','Days CO','Days NO2','Days Ozone',
                'Days SO2','Days PM2.5','Days PM10']
